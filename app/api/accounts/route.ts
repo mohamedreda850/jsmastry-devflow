d61cda46cac19189d2dc2da602adb962c1892079
mohamed reda <mohamed.reda86060@gmail.com>
2025-04-14 00:01:42 +0200

    impelement Create and Read Account Route

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,36 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/vaildations";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+
+
+export async function GET() {
+  try {
+    await dbConnect();
+    const accounts = await Account.find();
+    return NextResponse.json({ succsess: true, data: accounts }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const validateData = AccountSchema.parse(body);
+  
+    const existingAccount = await Account.findOne({ 
+        provider: validateData.provider,
+        providerAccountId: validateData.providerAccountId
+     });
+    if (existingAccount) throw new ForbiddenError("An account with the same provider already exists");
+
+    const newAccount = await Account.create(validateData);
+    return NextResponse.json({ success: true, data: newAccount }, { status: 201 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
